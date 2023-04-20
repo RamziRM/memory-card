a8e8b349283a8f083c3b1c702ce59ebee1e196d4
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-20 01:17:47 -0300

    update card - fixed passed link

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
-// import "./Card.css";
+import React from "react";
+
 
 // destructured object as parameter
-// (would be the same as props.title and props.image)
-const Card = ({ title, image }) => {
-    
-    const handleClick = () => {
-        console.log("card clicked");
-    };
+// (would be the same as props.card and props.shuffleCards)
+const Card = ({ card, shuffleCards }) => {
+  const { title, thumbnailUrl } = card;
 
-    return (
-        <div className="card" onClick={handleClick}>
-        <div className="card-image">
-            <img src={image} alt={title} />
-        </div>
-        <div className="card-content">
-            <h2>{title}</h2>
-        </div>
-        </div>
-    );
-}
+  const handleClick = () => {
+    shuffleCards();
+  };
+
+  return (
+    <div className="card" onClick={handleClick}>
+      <div className="card-image">
+        <img src={thumbnailUrl} alt={title} />
+      </div>
+      <div className="card-content">
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
