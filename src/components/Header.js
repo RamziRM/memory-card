e8ca9d1eeffe710f3285691f429f2bb92c46fd63
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-29 01:07:04 -0300

    added header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Header = ({score, highScore}) => {
     return (
-        <div className="header-cont">
-            <div className="header-logo">
-                <h1>CARD Memory game</h1>
+        <div className="flex justify-between font-mono text-4xl p-3 px-5 bg-blue-200">
+            <div className="">
+                <h1>Card Memory game</h1>
             </div>
             <div className="score">
                 <h2>Current Score: {score}</h2>
             </div>
-            <div className="high-score">"
+            <div className="high-score">
                 <h2>High Score: {highScore}</h2>
             </div>
         </div>
