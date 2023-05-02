760de2793d2be68ab63427327f15a0e243010013
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-02 01:16:11 -0300

    optimized for mobile + styling

--- src/components/CContainer.js
+++ src/components/CContainer.js
@@ -1,48 +1,51 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import { images } from "./imageLoader";
 
 const CContainer = ({ handleGameLogic }) => {
-    const [cards, setCards] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setLoading(true);
-        // Instead of fetching images from an API, use the imported images
-        const data = images.map((src, i) => ({ id: i + 1, title: `Image ${i + 1}`, url: src }));
-        // Shuffle the cards/photos
-        for (let i = data.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [data[i], data[j]] = [data[j], data[i]];
-        }
-        setCards(data);
-        setLoading(false);
-      }, []);
-      
+  useEffect(() => {
+    setLoading(true);
+    // Instead of fetching images from an API, use the imported images
+    const data = images.map((src, i) => ({
+      id: i + 1,
+      title: `Image ${i + 1}`,
+      url: src,
+    }));
+    // Shuffle the cards/photos
+    for (let i = data.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [data[i], data[j]] = [data[j], data[i]];
+    }
+    setCards(data);
+    setLoading(false);
+  }, []);
 
-    const shuffleCards = () => {
-        const shuffledCards = cards.sort(() => Math.random() - 0.5);
-        setCards([...shuffledCards]);
-      };
-    
-    return (
-        <div className="">
-            {loading ? (
-            <h1>Loading...</h1>
-            ) : (
-            <div className="grid grid-cols-4 bg-gray-300">
-                {cards.map((card, i) => (
-                <Card
-                    key={card.id}
-                    card={card}
-                    handleGameLogic={handleGameLogic}
-                    shuffleCards={shuffleCards}
-                />
-                ))}
-            </div>
-            )}
+  const shuffleCards = () => {
+    const shuffledCards = cards.sort(() => Math.random() - 0.5);
+    setCards([...shuffledCards]);
+  };
+
+  return (
+    <div className="">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-4 bg-gray-300">
+          {cards.map((card, i) => (
+            <Card
+              key={card.id}
+              card={card}
+              handleGameLogic={handleGameLogic}
+              shuffleCards={shuffleCards}
+            />
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default CContainer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Header = ({ score, highScore }) => {
   return (
-    <div className="flex justify-between font-mono text-4xl p-3 px-5 bg-blue-200">
-      <div className="">
-        <h1>Card Memory game</h1>
+    <div className="flex justify-between font-mono text-4xl p-3 px-5 bg-blue-900 text-white items-center">
+      <div className="hidden text-orange-600 text-2xl md:inline">
+        <h1>Memory?</h1>
       </div>
-      <div className="score">
+      <div className="text-2xl md:text-3xl">
         <h2>Current Score: {score}</h2>
       </div>
-      <div className="high-score">
+      <div className="text-xl md:text-3xl">
         <h2>High Score: {highScore}</h2>
       </div>
     </div>
