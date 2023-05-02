cf7a637cca9f9ed47fa58f0a0c300b77ad144821
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-02 00:16:50 -0300

    prettier?

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const Header = ({score, highScore}) => {
-    return (
-        <div className="flex justify-between font-mono text-4xl p-3 px-5 bg-blue-200">
-            <div className="">
-                <h1>Card Memory game</h1>
-            </div>
-            <div className="score">
-                <h2>Current Score: {score}</h2>
-            </div>
-            <div className="high-score">
-                <h2>High Score: {highScore}</h2>
-            </div>
-        </div>
-    );
-}
+const Header = ({ score, highScore }) => {
+  return (
+    <div className="flex justify-between font-mono text-4xl p-3 px-5 bg-blue-200">
+      <div className="">
+        <h1>Card Memory game</h1>
+      </div>
+      <div className="score">
+        <h2>Current Score: {score}</h2>
+      </div>
+      <div className="high-score">
+        <h2>High Score: {highScore}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
