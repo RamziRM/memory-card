3973fcc7399728795e72d8d6a193d45c0c951573
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-06 01:29:31 -0300

    logo - reload pg onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Header = ({ score, highScore }) => {
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="fixed w-full flex justify-between font-mono text-4xl p-3 px-5 bg-blue-900 text-white items-center">
       <div className="hidden text-orange-600 text-2xl md:inline">
-        <h1>Memory?</h1>
+        <h1 onClick={handleRefresh} className="hover:cursor-pointer">
+          Memory?
+        </h1>
       </div>
       <div className="text-2xl md:text-3xl">
         <h2>Current Score: {score}</h2>
